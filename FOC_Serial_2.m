dd0d3f7ad844c1af65da8f83f8389ee80b93bb10
kapis20 <kacper.filip.sikorski@gmail.com>
2024-03-12 14:55:50 +0000

    Serial communication sorted out in the script. Noise is removed and appropiate scalling factor multiplied. The app was updated by some new structures, such as radio button for operation. Also adequate operation messages asre stored based on the user choice to be sent later to C2000 board

--- FOC_Serial_2.m
+++ FOC_Serial_2.m
@@ -4,7 +4,7 @@ device = serialport("COM13",12e6);
 
 %% messages 
 SpeedRPM = single(-600);
-enable = single(16);
+enable = single(80);
 
 SpeedRPM = SpeedRPM * 1/PU_System.N_base;
 
@@ -30,13 +30,7 @@ AllElements = prod(matrixsize);
 % Define time interval between samples
 timeInterval = 1 / 12e6; % Time interval between samples in seconds
 
-% %Pre-allocate data arrays
-% DataA = zeros(matrixsize(1), 1);
-% DataB = zeros(matrixsize(1), 1);
-% 
 
-% Pre-calculate time vector
- %time = linspace(0, timeInterval*(matrixsize(1)-1), matrixsize(1));
  figure;
  %hLine = plot(NaN(matrixsize(1), 2));  % Initialize an empty plot with 2 lines
  hLine = plot(linspace(0,1200,1200),NaN(1200, 2) );  % Initialize an empty plot with 2 lines
@@ -53,19 +47,14 @@ try
   while true
     data = read(device, AllElements, 'single');
       % Extract DataA and DataB from received data and concatenate with existing arrays
-        DataA = [DataA, data(1:2:end) .* PU_System.N_base];
-        DataB = [DataB, data(2:2:end) .* PU_System.N_base];
-    % Extract data
-    % DataA = data(1:2:end) .* PU_System.N_base;
-    % DataB = data(2:2:end) .* PU_System.N_base;
-    if numel(DataA) == 1200
-    % Calculate time vector for the current data
-    %time = linspace(0, (matrixsize(1) - 1) * timeInterval, 2*matrixsize(1));
-    
-     % Ensure DataA and DataB are vectors (if needed)
-      % DataA = DataA(:);  % Extract first column if necessary
-      % DataB = DataB(:);
-      % Update plot data and time vector
+      %Mode1
+        % DataA = [DataA, data(1:2:end) .* PU_System.N_base];
+        % DataB = [DataB, data(2:2:end) .* PU_System.N_base];
+    %Mode 5
+      DataA = [DataA, data(1:2:end) .* PU_System.I_base];
+      DataB = [DataB, data(2:2:end) .* 2*pi];
+
+    if numel(DataA) == 2000
     DataA = rmoutliers(DataA);
     DataB = rmoutliers(DataB);
     set(hLine(1), 'XData', linspace(0,numel(DataA),numel(DataA)), 'YData', DataA);
@@ -75,13 +64,6 @@ try
     DataA = [];
     DataB = [];
     end
-    % % Update plot data efficiently
-    % set(hLine(1), 'YData', DataA);
-    % set(hLine(2), 'YData', DataB);
-
-    % % Force MATLAB to update the plot
-    % drawnow;
-    % pause(0.01);  % Adjust pause time as needed
   end
 catch ME
   clear device;
